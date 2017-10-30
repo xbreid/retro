df01a7ce50adb60fd30d2f2ed1b2e4b432463e35
Brandon Reid <brandonreid2@my.unt.edu>
2017-10-29 20:47:42 -0500

    finished 404 page (for now)

--- src/views/pages/404/index.js
+++ src/views/pages/404/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
-
-/*// styling
-import './style.scss';*/
+import {Link} from 'react-router';
+import './style.scss';
 
 export default () => (
-  <div>
-    404
+  <div className="error-wrapper">
+    <div className="content">
+      <Link to="/"/>
+    </div>
   </div>
 );
