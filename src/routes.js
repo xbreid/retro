1f909137441e70bfc17641344438ed4a73466d0b
Brandon Reid <brandonreid2@my.unt.edu>
2017-10-29 20:45:31 -0500

    updated routes from page deletes

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,8 @@ import React from 'react';
 import {Route, IndexRoute, Redirect} from 'react-router';
 import App from './App';
 import Vault from './views/vault';
-import Notes from './views/notes';
 import Home from './views/home';
-import Tools from './views/tools';
-import Settings from './views/settings';
+import Generator from './views/generator';
 import Page404 from './views/pages/404';
 import Callback from './callback/Callback';
 import { requireAuth } from './auth/AuthService';
@@ -15,9 +13,7 @@ export default (
     <Route component={App} path='/'>
       <IndexRoute component={Home}/>
       <Route path='vault' component={Vault} onEnter={requireAuth}/>
-      <Route path='notes' component={Notes} onEnter={requireAuth}/>
-      <Route path='tools' component={Tools} onEnter={requireAuth}/>
-      <Route path='settings' component={Settings} onEnter={requireAuth}/>
+      <Route path='generator' component={Generator} onEnter={requireAuth}/>
     </Route>
     <Route path="callback" component={Callback}/>
     <Route path="404" component={Page404}/>
