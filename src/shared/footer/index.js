981e3fdb4bb493e4e16b4d56bdf4e64d430b87f6
Brandon Reid <brandonreid2@my.unt.edu>
2017-10-30 00:07:52 -0500

    added github icon to footer

--- src/shared/footer/index.js
+++ src/shared/footer/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import IconGit from 'react-icons/lib/fa/github';
 
 // style
 import './style.scss';
@@ -17,10 +18,18 @@ const link = {
   color: 'white'
 };
 
+const git = {
+  textDecoration: 'none',
+  color: 'white'
+};
+
 export default () => (
   <div className="footer-container">
     <div style={blankBlock}/>
     <p style={footerText}>ReactPM is developed by <a style={link} href="http://brandonreid.io">Brandon Reid</a> in Denton, Texas.</p>
+    <a style={git} href="https://www.github.com/bar0191/retro">
+      <IconGit size="27" />
+    </a>
     <div style={blankBlock}/>
   </div>
 );
